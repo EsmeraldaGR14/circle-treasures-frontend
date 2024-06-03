25bf32cbaa4fef790d9ecaa3b09da15ad37db0ac
Esmeralda Gonzalez Rojo <esmeralda@esmeraldas-air.mynetworksettings.com>
2024-06-03 16:42:55 -0400

    added basic structure for the app.js file including react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
